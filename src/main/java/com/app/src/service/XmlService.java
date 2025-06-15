744c2558b016efbf4388b1007fe8432f1f42c7a0
NevaxN <arthurmayersantos@gmail.com>
2025-06-15 19:04:22 -0300

    Adicionando funcionalidade onde após enviar xml o pesquisador terá seus dados lidos e adicionados no seu cadastro

--- src/main/java/com/app/src/service/XmlService.java
+++ src/main/java/com/app/src/service/XmlService.java
@@ -1,10 +1,18 @@
 package com.app.src.service;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
+
+import com.app.src.model.Pesquisador;
+import com.app.src.model.Usuario;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class XmlService {
@@ -17,5 +25,37 @@ public class XmlService {
             return matcher.group(1);
         }
         return "UTF-8"; // fallback padrão
+    }
+
+    public Pesquisador converterJsonParaPesquisador(String jsonBody, Usuario usuario) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(jsonBody);
+            JsonNode dados = root.get("dados_pesquisador");
+
+            DateTimeFormatter dataFormatter = DateTimeFormatter.ofPattern("ddMMyyyy");
+            DateTimeFormatter horaFormatter = DateTimeFormatter.ofPattern("HHmmss");
+
+            Pesquisador pesquisador = new Pesquisador();
+            pesquisador.setUsuario(usuario);
+            pesquisador.setLattesId(dados.get("numero_identificador").asLong());
+
+            pesquisador.setDataNascimento(LocalDate.parse(dados.get("data_nascimento").asText(), dataFormatter));
+            pesquisador.setDataAtualizacao(LocalDate.parse(dados.get("data_atualizacao").asText(), dataFormatter));
+            pesquisador.setHoraAtualizacao(LocalTime.parse(dados.get("hora_atualizacao").asText(), horaFormatter));
+
+            pesquisador.setNomeCitacoesBibliograficas(dados.get("citacoes_bibliograficas").asText());
+            pesquisador.setNacionalidade(dados.get("nacionalidade").asText());
+            pesquisador.setPaisNascimento(dados.get("pais_nascimento").asText());
+
+            String[] nomePartes = dados.get("nome_completo").asText().split(" ", 2);
+            pesquisador.setNomePesquisador(nomePartes[0]);
+            pesquisador.setSobrenome(nomePartes.length > 1 ? nomePartes[1] : "");
+
+            return pesquisador;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao converter JSON para Pesquisador", e);
+        }
     }
 }

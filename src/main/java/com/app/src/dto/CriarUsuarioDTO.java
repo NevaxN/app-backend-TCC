3e3b366b3b339b3bf201598edf3656c37f95be89
nevaxN <arthurmayersantos@gmail.com>
2025-09-09 14:25:19 -0300

    Adicionando lógica ao service para lidar com as permissões vindas do JSON

--- src/main/java/com/app/src/dto/CriarUsuarioDTO.java
+++ src/main/java/com/app/src/dto/CriarUsuarioDTO.java
@@ -1,10 +1,7 @@
 package com.app.src.dto;
 
-import com.app.src.auth.models.RoleName;
-
-
 public record CriarUsuarioDTO(    
     String login,
     String password,
-    RoleName role) {
+    String role) {
 }

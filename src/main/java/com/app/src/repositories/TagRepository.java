3f994efca2ff2bad3f2c3f04fb33d494052ccc70
NevaxN <arthurmayersantos@gmail.com>
2025-10-24 15:42:13 -0300

    Criando lógica de busca de Pesquisadore por nome da tag

--- src/main/java/com/app/src/repositories/TagRepository.java
+++ src/main/java/com/app/src/repositories/TagRepository.java
@@ -1,5 +1,6 @@
 package com.app.src.repositories;
 
+import com.app.src.models.Pesquisador;
 import com.app.src.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -26,4 +27,8 @@ public interface TagRepository extends JpaRepository<Tag, Integer> {
     // Novo método para busca por tag
     @Query("SELECT t FROM Tag t JOIN t.listaTags tag WHERE LOWER(tag) LIKE LOWER(CONCAT('%', :termo, '%'))")
     List<Tag> findByTagContaining(@Param("termo") String termo);
+
+    // Achar Pesquisadores Por Nome da Tag
+    @Query("SELECT DISTINCT t.pesquisador FROM Tag t JOIN t.listaTags tagString WHERE tagString = :tagName")
+    List<Pesquisador> findPesquisadoresByTagName(@Param("tagName") String tagName);
 }

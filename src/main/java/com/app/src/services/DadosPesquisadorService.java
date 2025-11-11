f5decfad6d7207b5d2a69df6fd15736d2dd8e70b
Giovanni Policene <giovannicpoli@gmail.com>
2025-11-10 22:19:14 -0300

    feat: Atualizando DadosPesquisadorService

--- src/main/java/com/app/src/services/DadosPesquisadorService.java
+++ src/main/java/com/app/src/services/DadosPesquisadorService.java
@@ -3,6 +3,7 @@ package com.app.src.services;
 import com.app.src.dto.*;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -35,14 +36,50 @@ public class DadosPesquisadorService {
     public DadosPesquisadorDTO buscarDadosPesquisadorPorId(Integer idPesquisador) {
 
         PesquisadorDTO pesquisador = pesquisadorService.buscarPorId(idPesquisador);
-        List<FormacaoAcademicaDTO> formacoesAcademicas = formacaoAcademicaService.buscarPorIdPesquisador(idPesquisador);
+
+        List<FormacaoAcademicaDTO> formacoesAcademicas =
+                formacaoAcademicaService.buscarPorIdPesquisador(idPesquisador)
+                        .stream()
+                        .sorted(Comparator.comparingInt(FormacaoAcademicaDTO::getSequenciaFormacao))
+                        .limit(3)
+                        .toList();
+
+
         List<IdiomaDTO> idiomas = idiomaService.buscarPorIdPesquisador(idPesquisador);
-        List<AtuacaoProfissionalDTO> atuacoesProfissionais = atuacaoProfissionalService.buscarPorIdPesquisador(idPesquisador);
+
+        List<AtuacaoProfissionalDTO> atuacoesProfissionais = atuacaoProfissionalService.buscarPorIdPesquisador(idPesquisador)
+                .stream()
+                .sorted(Comparator.comparingInt(AtuacaoProfissionalDTO::getSequenciaAtuacao))
+                .limit(3)
+                .toList();
+
         List<ProjetoPesquisaDTO> projetoPesquisas = projetoPesquisaService.buscarPorIdPesquisador(idPesquisador);
-        List<ArtigoDTO> artigos = artigoService.buscarPorIdPesquisador(idPesquisador);
-        List<LivroDTO> livros = livroService.buscarPorIdPesquisador(idPesquisador);
-        List<CapituloDTO> capitulos = capituloService.buscarPorIdPesquisador(idPesquisador);
-        List<TrabalhoEventoDTO> trabalhosEventos = trabalhoEventoService.buscarPorIdPesquisador(idPesquisador);
+
+        List<ArtigoDTO> artigos = artigoService.buscarPorIdPesquisador(idPesquisador)
+                .stream()
+                .sorted(Comparator.comparingInt(ArtigoDTO::sequenciaProducao))
+                .limit(3)
+                .toList();
+
+        List<LivroDTO> livros = livroService.buscarPorIdPesquisador(idPesquisador)
+                .stream()
+                .sorted(Comparator.comparingInt(LivroDTO::sequenciaProducao))
+                .limit(3)
+                .toList();
+
+        List<CapituloDTO> capitulos = capituloService.buscarPorIdPesquisador(idPesquisador)
+                .stream()
+                .sorted(Comparator.comparingInt(CapituloDTO::sequenciaProducao))
+                .limit(3)
+                .toList();
+
+
+        List<TrabalhoEventoDTO> trabalhosEventos = trabalhoEventoService.buscarPorIdPesquisador(idPesquisador)
+                .stream()
+                .sorted(Comparator.comparingInt(TrabalhoEventoDTO::sequenciaProducao))
+                .limit(3)
+                .toList();
+
         TagDTO tags = tagService.buscarPorIdPesquisador(idPesquisador);
 
 
